b6df8d7b4fcb1155372063ab883bfb1236287d29
justinetroyke <jjtroyke@gmail.com>
2018-03-19 14:39:03 -0600

    initial commit

---
+++ lib/node.rb
@@ -0,0 +1,15 @@
+class Node
+
+attr_reader :sound
+
+  def initialize(sound)
+    @sound = sound
+  end
+
+  def data
+    @sound
+  end
+
+  def next_node
+  end 
+end
---
+++ test/node_test.rb
@@ -0,0 +1,27 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require "./lib/node"
+require 'pry'
+
+class NodeTest < Minitest::Test
+
+  def test_it_exists
+    node = Node.new("plop")
+
+    assert_instance_of Node, node
+  end
+
+  def test_data_can_be_entered
+    node = Node.new("plop")
+
+    assert_equal "plop", node.data
+  end
+
+  def test_that_next_node_exists
+    node = Node.new("plop")
+
+    assert_equal nil, node.next_node
+  end
+end
